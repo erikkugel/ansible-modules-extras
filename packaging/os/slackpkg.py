640af2cddab6bb0ee908f897b954e038a341cb41
erikkugel <erik.kugel@gmail.com>
2015-01-02 13:13:35 -0500

    Created an Ansible module for engaing Slackpkg

---
+++ packaging/os/slackpkg.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+import shlex
+import subprocess
+import json
+
+# (http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python)
+#class bcolors:
+#	HEADER = '\033[95m'
+#	OKBLUE = '\033[94m'
+#	OKGREEN = '\033[92m'
+#	WARNING = '\033[93m'
+#	FAIL = '\033[91m'
+#	ENDC = '\033[0m'
+
+slackpkg = "/usr/sbin/slackpkg"
+
+# (http://docs.ansible.com/developing_modules.html#reading-input)
+args_file = sys.argv[1]
+args_data = file(args_file).read()
+arguments = shlex.split(args_data)
+
+## (https://docs.python.org/2.7/library/argparse.html)
+#parser = argparse.ArgumentParser(description='This is a Python wrapper around Slackpkg')
+#parser.add_argument('-a','--action', help='Action',required=True)
+#args = parser.parse_args()
+
+# (https://docs.python.org/2/library/subprocess.html)
+(stdout, stderr) = subprocess.Popen(slackpkg + " " + arguments[0], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+print json.dumps({
+	"action": arguments[0],
+	"stdout": stdout,
+	"stderr": stderr
+})
