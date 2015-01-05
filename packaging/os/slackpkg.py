f488a01e5cd9a89658666780fd34323d217ad5f6
erikkugel <erik.kugel@gmail.com>
2015-01-04 22:10:31 -0500

    Added wrappaer for slackpkg

--- packaging/os/slackpkg.py
+++ packaging/os/slackpkg.py
@@ -2,19 +2,27 @@
 
 import sys
 
-# check for updates, reutrn False if none available, return output if updates are available:
-def checkUpdates(slackpkgPath, slackpkgFlags):
-	(stdout, stderr) = subprocess.Popen(slackpkgPath + ' ' + slackpkgFlags + ' check-updates', \
+# run *anything* slackpkg supports by taking a slackpkg action and a package name (can be left blank)
+def slackpkgAux(slackpkgAction, slackpkgPackage):
+	(stdout, stderr) = subprocess.Popen(slackpkgPath + ' ' \
+		+ slackpkgFlags + ' ' \
+		+ slackpkgAction + ' ' \
+		+ slackpkgPackage, \
 		shell=True, stdout=subprocess.PIPE).communicate()
-	print stdout
-	if 'No news is good news' in stdout:
+	return stdout
+
+# check for updates, reutrn True if available:
+def checkUpdates():
+	if 'No news is good news' in slackpkgAux('check-updates', ''):
 		return False
 	else:
-		return (stdout)
+		return True
 
 # upgrade everything: update repositories, install new packages, upgrade existing packages, clean system
-def upgradeAll(slackpkgPath, slackpkgFlags):
-	print 'Upgrading everything'
+def upgradeAll():
+	return slackpkgAux('update', '') \
+		+ slackpkgAux('install-new', '') \
+		+ slackpkgAux ('upgrade-all', '')
 
 def main():
 	module = AnsibleModule(
@@ -24,20 +32,28 @@ def main():
 		),
 	)
 
+	# Get action and package from module parameters
 	action = module.params.get('action')
 	package = module.params.get('package')
+	if package is None:
+		package = ''
 
 	# path to slackpkg script
+	global slackpkgPath 
 	slackpkgPath = '/usr/sbin/slackpkg'
+
 	# flags for slackpkg, batch=on and -default_answer=y are a must execution via Ansible
+	global slackpkgFlags
 	slackpkgFlags = '-batch=on -default_answer=y -checksize=on'
 
+	# call appropriate function
 	if action == 'check-updates':
-		module.exit_json(changed=False, updates=checkUpdates(slackpkgPath, slackpkgFlags))
-
-	if action == 'upgrade-all':
+		module.exit_json(changed=False, updates=checkUpdates())
+	elif action == 'upgrade-all':
 		module.exit_json(changed=True, output=upgradeAll())
+	else:
+		module.exit_json(changed=True, output=slackpkgAux(action, package))
+
 # import module snippets
 from ansible.module_utils.basic import *
-
 main()
